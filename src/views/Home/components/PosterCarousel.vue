93e52468808997664618ca9d588e98c21bae1f22
Ana Verena Gomes <anaverena.hrzon@gmail.com>
2023-10-12 22:37:40 -0300

    feature(PosterCarousel): implementes carousel component for movies posters

---
+++ src/views/Home/components/PosterCarousel.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import PosterCard from './PosterCard.vue';
+//TODO: adicionar tipagem de movieList
+defineProps(['movieList'])
+</script>
+
+<template>
+    <v-carousel :show-arrows="false" hide-delimiter-background height="100%" continuous cycle color="text-secondary">
+        <v-carousel-item v-for="(movie, i) in movieList" :key="i" :src="movie.img" cover max-height="100vh"
+            class="movie-poster" height="100vh">
+            <PosterCard :movie="movie"/>
+        </v-carousel-item>
+    </v-carousel>
+</template>
