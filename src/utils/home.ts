499ed74d8f47c29c0458f47c750678cee9b0f6b6
Ana Verena Gomes <anaverena.hrzon@gmail.com>
2023-10-12 22:39:38 -0300

    feature(HomePage): implements home page

---
+++ src/utils/home.ts
@@ -0,0 +1,20 @@
+export const homeShape = [
+    {
+        movie: 'Barbie',
+        img: 'https://m.media-amazon.com/images/M/MV5BM2Q3MzlkNDItODhmMS00NmFiLWIzZjctOGRlZDhjMTgyNzZjXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX2160_.jpg',
+        about: 'Barbie and Ken are having the time of their lives in the colorful and seemingly perfect world of Barbie Land. However, when they get a chance to go to the real world, they soon discover the joys and perils of living among humans.',
+        year: '2023',
+        genre: 'Adventure',
+        duration: '114',
+        range: '7.2'
+    },
+    {
+        movie: 'Interstellar',
+        img: 'https://m.media-amazon.com/images/M/MV5BOGZlM2I1NDQtMWFlNS00Y2FmLWFmYzAtYTE5OWVlZGVlYzM3XkEyXkFqcGdeQXVyMTE0NTczNDAz._V1_FMjpg_UX1024_.jpg',
+        about: 'The adventures of a group of explorers who make use of a newly discovered wormhole to surpass the limitations on human space travel and conquer the vast distances involved in an interstellar voyage',
+        year: '2023',
+        genre: 'Adventure',
+        duration: '114',
+        range: '7.2'
+    }
+]
---
+++ src/views/Home/HomePage.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import PosterCarousel from './components/PosterCarousel.vue'
+import { homeShape } from '../../utils/home';
+
+console.log('oi')
+</script>
+
+<template>
+    <v-container style="max-width: 100%;" class="fluid w-100 mx-0 px-0 h-screen py-0">
+        <v-row>
+            <v-col cols="12" class="py-0">
+                <PosterCarousel :movie-list="homeShape"/>
+            </v-col>
+        </v-row>
+    </v-container>
+
+    
+</template>
+
+<style>
+.movie-poster {
+    position: relative;
+}
+
+.movie-poster--card {
+    /* font-family: 'IBM Plex Sans Arabic', sans-serif; */
+    font-family: 'Urbanist', sans-serif;
+    bottom: 20px;
+    left: 120px;
+}
+</style>
