ccd5478830260e75c9e9161ac0ae1cf7bb0b1cdb
Ana Verena Gomes <anaverena.hrzon@gmail.com>
2023-10-15 20:09:58 -0300

    feature(MovieCard): creates movie display cards for carousel in home page

---
+++ src/views/Home/components/MovieCard.vue
@@ -0,0 +1,35 @@
+
+<script setup lang="ts">
+import router from '../../../router';
+
+
+defineProps(['movie', 'selectedClass', 'toggle'])
+
+const redirectToDetails = (id: string) => {
+    router.push(`/details/${id}`)
+}
+
+const imageUrlBase = 'http://image.tmdb.org/t/p/w300';
+
+</script>
+
+<template>
+    <v-hover>
+        <template v-slot:default="{ isHovering, props }">
+            <v-card :class="['ma-4', selectedClass, 'sssss']" height="200" width="140" v-if="movie.backdrop_path"
+                :elevation="isHovering ? 12 : 2" @click="redirectToDetails(movie.id)">
+                <v-card-item class="w-100 h-100 px-0">
+                    <v-img :src="`${imageUrlBase}${movie.poster_path}`" cover min-width="100%" height="200" class="px-0" />
+                </v-card-item>
+            </v-card>
+        </template>
+    </v-hover>
+</template>
+
+<style>
+.sssss:hover {
+    transform: scale(1.1);
+    transition: transform 0.3s ease;
+
+}
+</style>
