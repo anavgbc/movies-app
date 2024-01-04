5610447f84e77af90e75f5754b018cbd9d3ba778
Ana Verena <anaverena.hrzon@gmail.com>
2024-01-04 19:30:01 -0300

    feature(LayoutHeader): implements redirect functions

--- src/components/LayoutHeader.vue
+++ src/components/LayoutHeader.vue
@@ -5,15 +5,27 @@ const redirectToHome = () => {
     router.push('/home')
 }
 
+const redirectToMovies = () => {
+    router.push('/movies')
+}
+
+const redirectToTvShows = () => {
+    router.push('/tvShows')
+}
+
+const redirectToFavoritesList = () => {
+    router.push('/favorites')
+}
+
 </script>
 <template>
     <v-app-bar class="bg-dark-1" flat scroll-behavior="hide">
-        <v-app-bar-title>Filmio</v-app-bar-title>
+        <v-app-bar-title @click="redirectToHome">Filmio</v-app-bar-title>
         <v-btn @click="redirectToHome">Home</v-btn>
-        <v-btn>Movies</v-btn>
-        <v-btn>Tv shows</v-btn>
+        <v-btn @click="redirectToMovies">Movies</v-btn>
+        <v-btn @click="redirectToTvShows">Tv shows</v-btn>
         <v-btn>Popular</v-btn>
-        <v-btn>Favorites</v-btn>
+        <v-btn @click="redirectToFavoritesList">Favorites</v-btn>
         <v-col cols="3">
             <v-text-field type="text" class="px-4" variant="outlined" hide-details density="compact"
                 prepend-inner-icon="mdi-magnify" label="Search" placeholder="Your favorite movie" />
