6bcd5a352553a86e90614d7630a92bbace8febfe
Ana Verena <anaverena.hrzon@gmail.com>
2024-01-04 17:49:48 -0300

    refactor(store/tvShows.ts): implements tvshows selection

--- src/store/tvShows.ts
+++ src/store/tvShows.ts
@@ -1,10 +1,16 @@
 import { defineStore } from 'pinia';
 import { TvShowShape } from '../types/models/tvShows/tvShows.types';
+import { Actor } from '../types/models/cast';
 
 export const useTvShowStore = defineStore('tvShow', {
     state: () => ({
         popularTvShows: [] as TvShowShape[],
         topRatedTvShows: [] as TvShowShape[],
+        selectedShow: {} as TvShowShape,
+        selectedShowImages: [] as any,
+        selectedShowCast: [] as any,
+        selectedShowSimilar: [] as any,
+        selectedShowReviews: [] as any,
     }),
     actions: {
         setPopularTvShows(tvShows: TvShowShape[]) {
@@ -12,6 +18,27 @@ export const useTvShowStore = defineStore('tvShow', {
         },
         setTopRatedTvShows(tvShows: TvShowShape[]) {
             this.topRatedTvShows = tvShows
-        }
+        },
+        setSelectedShow(show: TvShowShape) {
+            this.selectedShow = show;
+        },
+        setSelectedShowImages(images: {
+            aspect_ratio: number;
+            height: number;
+            width: number;
+            vote_average: number;
+            vote_count: number;
+        }[]) {
+            this.selectedShowImages = images;
+        },
+        setSelectedShowCast(castList: Actor[]) {
+            this.selectedShowCast = castList;
+        },
+        setSelectedShowSimilar(similarList: any[]) {
+            this.selectedShowSimilar = similarList;
+        },
+        setSelectedShowReview(reviewList: any[]) {
+            this.selectedShowReviews = reviewList;
+        },
     }
 })
